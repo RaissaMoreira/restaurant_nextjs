f5dd605a3ce317c8847870afa73bb0eb7301a9a9
RaissaMoreira <raissamoreira80@gmail.com>
2024-10-15 18:51:07 -0300

    ajustes requisição

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-let apiBaseUrl = 'http://192.168.1.9:3001';
-// let apiBaseUrl = `${window.location.hostname}:3001`;
-// const apiBaseUrl = process.env.API_BASE_URL;
-console.log("aaaa ",apiBaseUrl);
+let apiBaseUrl = 'http://127.0.0.1:3001';
 
 const api = axios.create({
   baseURL: apiBaseUrl,
